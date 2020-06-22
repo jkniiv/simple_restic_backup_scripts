a22a41c3a6026ed5cf233ce28dff52db2dba8dc5
Jarkko Kniivila <jkniiv@gmail.com>
2020-06-22 10:29:00 +0300

    Clarify how Restic's forget/keep policies work

--- bin/restic-forget-according-to-my-policy.sh
+++ bin/restic-forget-according-to-my-policy.sh
@@ -22,8 +22,18 @@ pwfile=c:/Users/myusernameunderwindows/Documents/my-secret-for-something--really
 # the same you use with 'restic backup' if you want the 'restic forget' operations to target the same dataset.
 host=eff74b7e-aaaa-bbbb-cccc-eb5ccexxxxxx
 
-# Tailor this invocation accordingly. Here I tee all restic output to a log file under ~/restic_recent_logs
-# This 'forget' operation makes sure to keep 14 daily, 6 weekly, 18 monthly and 8 yearly backups.
+# Tailor the following invocation accordingly. Here I tee all restic output to a log file under ~/restic_recent_logs
+# This 'forget' operation makes sure to keep 14 daily, 8 weekly, 20 monthly and 9 yearly backups.
+#
+# N.B. When it comes to the weekly, monthly and yearly period, remember that Restic considers the last
+# backup of that (calendar) period the one to keep (with Sunday as the *last* day of the week), with the most
+# recent backup *also* being the very latest weekly, monthly and yearly backup. So for instance with N weekly backups
+# you are only able to restore files at most N-1 weeks back and on Mondays closer to N-2 weeks back (because
+# the current day also consumes a "keep"). This may feel unintuitive at first.
+#
+# TL;DR: Always make sure to add 2 to your estimates as to how far your backup you needs to be able to recover files.
+# The policy below makes sure you can go *at least* 6 weeks back in weeklies, 18 months back in monthlies and finally
+# at least 7 years back in terms of yearlies.
 ${restic} -r "${repo}" -p "${pwfile}" --verbose=4 \
-    forget --host "${host}" --keep-daily 14 --keep-weekly 6 --keep-monthly 18 --keep-yearly 8 2>&1 \
+    forget --host "${host}" --keep-daily 14 --keep-weekly 8 --keep-monthly 20 --keep-yearly 9 2>&1 \
     | tee "${HOME}/restic_recent_logs/forget.LOG"
